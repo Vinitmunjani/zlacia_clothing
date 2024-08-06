7eb3f5eb6d12e8a1bd199b1a45fd8a14594cdf4f
Vinitmunjani <vinitmunjani1513@gmail.com>
2024-08-06 21:43:54 +0530

    done vercel final4

--- zlacia_clothing/settings.py
+++ zlacia_clothing/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'zlacia_clothing.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -105,7 +105,7 @@ DATABASES = {
     }
 }
 
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
